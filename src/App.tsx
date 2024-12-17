acd690a3e2578623a34be320fb73e33be76df3a7
harishveerapaneni <git@stackblitz.com>
2024-12-17 16:50:59 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  /*let selected = 1;*/
+  const [selected, setselected] = useState(
+    localStorage.getItem('selectedCourseIdx')
+  );
+
+  const courses = ['Html', 'Css', 'Bootstrap', 'JavaScript', 'React'];
+
+  const handleClick = (text, idx) => {
+    setselected(idx);
+  };
+
+  const typeCourseClick = (idx) => {
+    setselected(idx);
+    localStorage.setItem('selectedCourseIdx', idx);
+  };
+
+  return (
+    <>
+      <h2>Your selected course: {courses[selected]}</h2>
+      <ul>
+        {courses.map((value, idx) => {
+          return (
+            <li
+              className={selected === idx ? 'active' : ''}
+              onClick={() => typeCourseClick(idx)}
+            >
+              {value}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
